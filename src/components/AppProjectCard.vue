a7d9bd0082ecce0b04354911a4db666c18be2902
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-30 16:29:13 +0200

    created AppProjectCard.vue component, printed project title for test, project object passed via prop from AppMain

---
+++ src/components/AppProjectCard.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        name: 'AppProjectCard',
+
+        props: {
+            project: Object
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+<template>
+    <div id="project-card">
+        <h1>{{ project.title }}</h1>
+    </div>
+</template>
