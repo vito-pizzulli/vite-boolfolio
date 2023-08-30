125d91b97cef7872f1546811df5bc9cdf1d3b505
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-30 16:00:26 +0200

    imported axios in AppMain, added method getProjects that makes the api call, added created hook that executes the getProjects method when the app is created, projects data is printed in console

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,32 @@
 <script>
+    import axios from 'axios';
     export default {
         name: 'AppMain',
+
+        data() {
+            return {
+                projects: [],
+                apiUrl: 'http://127.0.0.1:8000/api/projects'
+            }
+        },
+
+        methods: {
+            getProjects() {
+                axios.get(this.apiUrl, {
+                    params: {}
+                })
+                .then(function (response) {
+                    console.log(response.data.results.data);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+            }
+        },
+
+        created() {
+            this.getProjects();
+        }
     }
 </script>
 
