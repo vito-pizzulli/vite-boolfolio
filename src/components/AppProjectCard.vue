2161a3afc12d6058dfd8081b089c3d259b4a1bde
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-30 17:21:07 +0200

    Bonus completed - each project received via api now has its informations printed inside a card

--- src/components/AppProjectCard.vue
+++ src/components/AppProjectCard.vue
@@ -9,11 +9,38 @@
 </script>
 
 <style lang="scss" scoped>
+    @use '../styles/partials/mixins' as *;
 
+    div#project-card {
+        @include flex(column, space-between, start, no-wrap);
+
+        background-color: white;
+        width: calc(100% / 3 - 2rem);
+        margin: 1rem;
+        padding: 1rem;
+        border-radius: 10px;
+
+        h1 {
+            font-size: 1.5rem;
+        }
+
+        div.project-technologies span {
+            margin-right: .5rem;
+        }
+    }
 </style>
 
 <template>
     <div id="project-card">
-        <h1>{{ project.title }}</h1>
+        <h1>Title: {{ project.title }}</h1>
+        <p><strong>Description:</strong> {{ project.description }}</p>
+        <p><strong>GitHub Page Link:</strong> {{ project.link }}</p>
+        <p><strong>Creation date:</strong> {{ project.creation_date }}</p>
+        <p><strong>Type:</strong> {{ project.type?.name ?? 'Empty' }}</p>
+        <div class="project-technologies">
+            <span><strong>Technologies:</strong></span>
+            <span v-for="technology in project.technologies">{{ technology.name }}</span>
+            <span v-if="!project.technologies.length">Empty</span>
+        </div>
     </div>
 </template>
