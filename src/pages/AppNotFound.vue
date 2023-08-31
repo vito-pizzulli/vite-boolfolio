ac892af0a37de53592248232fe5ce6f3a9cf1e6a
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-31 17:20:54 +0200

    created AppNotFound page that will be shown if the single project api call is not successful

---
+++ src/pages/AppNotFound.vue
@@ -0,0 +1,30 @@
+<script>
+    export default {
+        name: 'AppNotFound'
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/mixins' as *;
+    
+    main {
+        background-color: #41587e;
+        height: 75vh;
+        overflow-y: scroll;
+
+        div.container {
+            @include flex(row, center, space-between, wrap);
+
+            width: 80%;
+            margin: auto;
+        }
+    }
+</style>
+
+<template>
+    <main>
+        <div class="container">
+            <h1>Not Found</h1>
+        </div>
+    </main>
+</template>
