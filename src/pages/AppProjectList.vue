1b322976313299b7a754ef6b6dbaaba12acd6b26
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-08-31 14:50:17 +0200

    moved AppMain component's content into AppProjectList page, AppMain deleted

--- src/pages/AppProjectList.vue
+++ src/pages/AppProjectList.vue
@@ -1,15 +1,64 @@
 <script>
+    import AppProjectCard from '../components/AppProjectCard.vue';
+    import axios from 'axios';
     export default {
-        name:'AppProjectList'
+        name: 'AppProjectList',
+
+        components: {
+            AppProjectCard
+        },
+
+        data() {
+            return {
+                projects: [],
+                apiUrl: 'http://127.0.0.1:8000/api/projects'
+            }
+        },
+
+        methods: {
+            getProjects() {
+                axios.get(this.apiUrl, {
+                    params: {}
+                })
+                .then((response) => {
+                    console.log(response.data.results.data);
+                    this.projects = response.data.results.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+            }
+        },
+
+        created() {
+            this.getProjects();
+        }
     }
 </script>
 
+<style lang="scss" scoped>
+    @use '../styles/partials/mixins' as *;
+
+    main {
+        background-color: #41587e;
+        height: 80vh;
+        overflow-y: scroll;
+
+        div.container {
+            @include flex(row, center, space-between, wrap);
+
+            width: 80%;
+            margin: auto;
+        }
+    }
+</style>
+
 <template>
     <main>
-        <h1>Project List</h1>
+        <div class="container">
+            <AppProjectCard v-for="project in projects"
+                :project="project"
+            />
+        </div>
     </main>
 </template>
-
-<style lang="scss" scoped>
-    
-</style>
